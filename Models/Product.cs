1f487a3be8b3d4bc845ad720c691f4d569af4188
Medie <mmedie@gmail.com>
2019-01-13 00:19:32 +0100

    added search feild

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,21 +1,43 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-namespace MyWeb.Models
-{
-    public class Product
-    {
-        public int ID { get; set; }
-        public string Title { get; set; }
+namespace MyWeb.Models {
+        public class Product {
+            public Product (int iD, string title, DateTime releaseDate, string genre, decimal price, string rating) {
+                this.ID = iD;
+                this.Title = title;
+                this.ReleaseDate = releaseDate;
+                this.Genre = genre;
+                this.Price = price;
+                this.Rating = rating;
 
-        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
-        
-        [Display(Name = "Expire Date")]
-        [DataType(DataType.Date)]
-        public DateTime ReleaseDate { get; set; }
+            }
+            public int ID { get; set; }
 
-        public string Genre { get; set; }
-        public decimal Price { get; set; }
+            [StringLength (60, MinimumLength = 3)]
+            [Required]
 
-        public string Rating { get; set; }
-    }
+            public string Title { get; set; }
+
+            [Display (Name = "Expire Date")]
+            [DataType (DataType.Date)]
+
+            public DateTime ReleaseDate { get; set; }
+
+            [Range (1, 100)]
+            [DataType (DataType.Currency)]
+
+            public decimal Price { get; set; }
+
+            [RegularExpression (@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+            [Required]
+
+            [StringLength (30)]
+            public string Genre { get; set; }
+
+            [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+            [StringLength (5)]
+            [Required]
+
+            public string Rating { get; set; }
+        }
 }
