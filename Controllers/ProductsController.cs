72661f06da78891af0a0c4a04bf425793fdedd22
Meth <mmedie@gmail.com>
2019-02-24 18:11:38 +0100

    changes with Authentication

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -5,10 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using MyWeb.Models;
 
 namespace MyWeb.Controllers
 {
+    [Authorize]
     public class ProductsController : Controller
     {
         private readonly ProductContext _context;
--- Startup.cs
+++ Startup.cs
@@ -26,7 +26,7 @@ namespace MyWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //services.AddMvc();
             // using Microsoft.EntityFrameworkCore;
             services.AddDbContext<ProductContext>(options =>
                 options.UseSqlite("Data Source=Ticket.db"));
@@ -58,14 +58,17 @@ namespace MyWeb
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+            app.UseAuthentication();
             app.UseCookiePolicy();
 
-            app.UseMvc(routes =>
+            app.UseMvc
+            (routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+            
         }
     }
 }
